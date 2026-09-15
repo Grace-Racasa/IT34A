ce240425ef50a073f6ddf01d59ab99daa0b0c1a6
Grace-Racasa <racasa.grace1@gmail.com>
2026-09-15 13:42:01 +0800

    Added Activity logging and Signout

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require 'config/config.php';
 require 'config/function.php';
 
+
 if(isset($_SESSION['user_id'])){
     header('Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php');
     exit;
@@ -13,12 +14,34 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $login = trim($_POST['login'] ?? '');
     $password = $_POST['password'] ?? '';
 
-    if(loginUser($pdo, $login, $password)){
-        header('Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php');
-        exit;
-    }
 
-    $error = 'Invalid login credentials';
+
+
+    if($login===''||$password===''){
+        $error = 'Invalid login credentials';
+        logActivity(
+            $pdo, 
+            null, 
+            $login, 
+            'login', 
+            'failed'
+            );
+
+
+    } else {
+        if(loginUser($pdo, $login, $password)){
+            
+            logActivity(
+                $pdo,
+                $_SESSION['user_id'],
+                $_SESSION['user_email'],
+                'login',
+                'success'
+            );
+            header('Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php');
+            exit;
+        }
+    }
 }
 
 ?>
